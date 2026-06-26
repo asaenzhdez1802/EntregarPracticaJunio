75baf35a539b66f44b3060fe35314f48bc92a475
ItzelGuadalupeSanchezBautista <itzelsanchez73775@gmail.com>
2026-06-26 13:55:41 -0600

    Añadida clase Camioneta y soporte en Form1
    Se incorpora la clase Camioneta heredando de Vehiculo, con cálculo de tarifa específico. Se modifica Form1.cs para instanciar Camioneta según el tipo de vehículo seleccionado y se actualiza el archivo de proyecto para compilar Camioneta.cs.

---
+++ EntregarPracticaJunio/Camioneta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntregarPracticaJunio
+{
+    internal class Camioneta : Vehiculo
+    {
+        public int NumeroEjes { get; set; }
+
+        public Camioneta(int horasEstancia, int numeroEjes) : base(horasEstancia)
+        {
+            NumeroEjes = numeroEjes;
+        }
+
+        public override decimal CalcularTarifa()
+        {
+            return HorasEstancia * 45.00m;
+        }
+    }
+}
