e5d9807dcd51dc57e1526c317b2808d728f7b0e7
Angel_Angelino_Gonzalez <angelag020807@gmail.com>
2026-06-26 13:46:25 -0600

    se envia la clase Automovil

--- EntregarPracticaJunio/Automovil.cs
+++ EntregarPracticaJunio/Automovil.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace EntregarPracticaJunio
 {
-    internal class Automovil
+    internal class Automovil: Vehiculo
     {
+        public string Placa { get; set; }
+
+        public Automovil(int horasEstancia, string placa) : base(horasEstancia)
+        {
+            Placa = placa;
+        }
+
+        public override decimal CalcularTarifa()
+        {
+            return HorasEstancia * 30.00m;
+        }
     }
 }
